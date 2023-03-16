d1cc9318a751fed91c2f07b908fc83935eabc029
qtdoges <masterz4pl@gmail.com>
2023-03-16 06:36:30 +0300

    no comment

--- conflict.c
+++ conflict.c
@@ -4,10 +4,7 @@ int main() {
 // save three lines
     printf("conflict code! feature-1");
     printf("conflict code! main-");
-<<<<<<< HEAD
     printf("conflict code! main-1");
 
-=======
     printf("conflict code! main-3");
->>>>>>> feature
 }
